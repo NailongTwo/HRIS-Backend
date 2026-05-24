16c1a06ce046c0b1a9171f19920fce23250e4c39
Jay-D123 <rci.bsis.dometitajethro@gmail.com>
2026-05-24 22:05:53 +0800

    Fix: Add SSL to PostgreSQL connection for Render

--- config/db.js
+++ config/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect((err) => {
