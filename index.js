de621cdc845882c11c073cc5dc4bc82180504425
NailongTwo <cld101010002@gmail.com>
2026-06-01 18:32:02 +0800

    Fix CORS configuration

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
